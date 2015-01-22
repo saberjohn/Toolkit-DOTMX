34796b04371352def3a5befbdd41bc185f15177e
Mario Flores <pokazzam@gmail.com>
2015-01-21 23:20:10 -0600

    remove unnecessary files and views. Funcionality for scroll infinite added. New Design updated

--- new/scripts/app.js
+++ new/scripts/app.js
@@ -1,16 +1,17 @@
 'use strict';
 
-  angular.module('dotmxApp', [
-	'ngCookies',
-	'ngResource',
-	'ngSanitize',
-	'ui.router',
-	'ui.bootstrap'
-  ])
+	angular.module('dotmxApp', [
+		'ngCookies',
+		'ngResource',
+		'ngSanitize',
+		'ui.router',
+		'ui.bootstrap',
+		'duScroll'
+	]).value('duScrollDuration', 750)
 	.run(["$rootScope", "$state", "$stateParams",function ($rootScope, $state, $stateParams) {
 		$rootScope.$state = $state;
 		return $rootScope.$stateParams = $stateParams;
-  }])
+	}])
   .config(function ($stateProvider, $urlRouterProvider) {
 	//delete $httpProvider.defaults.headers.common['X-Requested-With'];
 	$urlRouterProvider.otherwise('/');
@@ -20,30 +21,5 @@
 		url: '/',
 		templateUrl: 'views/main.html',
 		controller:'MainCtrl'
-	  })
-	  .state('/diagnostic', {
-		url: '/diagnostico',
-		templateUrl: 'views/diagnostic.html',
-		controller: 'DiagnosticCtrl'
-	  })
-	  .state('/tools', {
-		url: '/herramientas',
-		templateUrl: 'views/tools.html',
-		controller: 'ToolsCtrl'
-	  })
-	  .state('/strategies', {
-		url: '/estrategias',
-		templateUrl: 'views/strategies.html',
-		controller: 'StrategiesCtrl'
-	  })
-	  .state('/model', {
-		url: '/modelo',
-		templateUrl: 'views/model.html',
-		controller: 'ModelCtrl'
-	  })
-	  .state('/about', {
-		url: '/acerca',
-		templateUrl: 'views/about.html',
-		controller: 'AboutCtrl'
-	  })
+	  });
   });
--- new/scripts/controllers/main.js
+++ new/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('dotmxApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope,$document) {
+    $scope.first = {
+      isFirstOpen: true
+    };
+	$document.on('scroll', function() {
+		var topPosition = angular.element(document.getElementById('topcontrol'));
+		if($document.scrollTop() >= 65){
+			topPosition.addClass('dark');
+		}
+		else{
+			topPosition.removeClass('dark');	
+		}
+	});
   });
