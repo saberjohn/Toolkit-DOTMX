80c111870f17eb433a2d5a9143731ac79145e556
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2015-01-27 17:48:39 -0600

    load map

--- new/scripts/controllers/main.js
+++ new/scripts/controllers/main.js
@@ -14,4 +14,8 @@ angular.module('dotmxApp')
 			topPosition.removeClass('dark');	
 		}
 	});
+	
+	L.mapbox.accessToken = 'pk.eyJ1IjoiY2Fhcmxvc2h1Z28xIiwiYSI6IklmZGNsNmMifQ.JJksWU3hBP-Vd3S9WtjFsA';
+	var map = L.mapbox.map('map-box', 'caarloshugo1.h9bggm26').setView([19.4313054168727, -99.1347885131836], 11);
+	
   });
