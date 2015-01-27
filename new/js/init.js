7fe9c3169d0d2ca99e77e8b43245c24a631c0783
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2015-01-27 17:46:19 -0600

    on load jquery

--- new/js/init.js
+++ new/js/init.js
@@ -1,2 +1,4 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoiY2Fhcmxvc2h1Z28xIiwiYSI6IklmZGNsNmMifQ.JJksWU3hBP-Vd3S9WtjFsA';
-var map = L.mapbox.map('map-box', 'caarloshugo1.h9bggm26').setView([19.4313054168727, -99.1347885131836], 11);
+$(document).ready( function () {
+	L.mapbox.accessToken = 'pk.eyJ1IjoiY2Fhcmxvc2h1Z28xIiwiYSI6IklmZGNsNmMifQ.JJksWU3hBP-Vd3S9WtjFsA';
+	var map = L.mapbox.map('map-box', 'caarloshugo1.h9bggm26').setView([19.4313054168727, -99.1347885131836], 11);
+});
