c3acba6384c2a78188162392c3a9d801f065dfd7
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2015-01-30 17:48:04 -0600

    indices sidebar

--- new/scripts/controllers/main.js
+++ new/scripts/controllers/main.js
@@ -53,20 +53,55 @@ angular.module('dotmxApp')
 	
 	function onEachFeature(feature, layer) {
 		layer.on('click', function(e) {
+			
+			estaciones.setStyle({
+				radius: 10,
+				fillColor: "#47c9af",
+				color: "#fff",
+				stroke: "#fff",
+				weight: 2,
+				dashArray: '2',
+				opacity: 1,
+				fillOpacity: 0.8
+			});
+			
+			this.setStyle({
+				color: '#2262CC',
+				weight: 3,
+				opacity: 0.6,
+				fillOpacity: 0.65,
+				fillColor: '#2262CC'
+			});
+                
 			circleLayer.clearLayers();
 			var circle = L.circle([layer._latlng.lat, layer._latlng.lng], 800, geojsonMarkerOptions2);
 			
 			//prueba
+			/*
 			var contenthtml = "<p>";
 				contenthtml += "Nombre: " + feature.properties.Name + "<br/>";
 				contenthtml += "Sum_PerR_1: " + feature.properties.Sum_PerR_1 + "<br/>";
 				contenthtml += "PobDesocup: " + feature.properties.PobDesocup + "<br/>";
 				contenthtml += "DensPobAvg: " + feature.properties.DensPobAvg + "<br/>";
 			contenthtml += "</p>";
+			*/
 			
 			//datos - falta meter labels correctos de las propiedades
 			$("#info-Name").html(feature.properties.Name);
+			$("#info-Descriptio").html(feature.properties.Descriptio);
 			$("#info-Sum_PerR_1").html(feature.properties.Sum_PerR_1);
+			$("#info-Sum_PerR_2").html(feature.properties.Sum_PerR_2);
+			$("#info-PerRemPrim").html(feature.properties.PerRemPrim);
+			$("#info-PerRemSecu").html(feature.properties.PerRemSecu);
+			$("#info-PerRemTerc").html(feature.properties.PerRemTerc);
+			$("#info-UEprim").html(feature.properties.UEprim);
+			$("#info-UEsecu").html(feature.properties.UEsecu);
+			$("#info-UEterc").html(feature.properties.UEterc);
+			$("#info-PobTot").html(feature.properties.PobTot);
+			$("#info-VivDeshab").html(feature.properties.VivDeshab);
+			$("#info-VivConAuto").html(feature.properties.VivConAuto);
+			$("#info-VivTodServ").html(feature.properties.VivTodServ);
+			$("#info-PobOcupada").html(feature.properties.PobOcupada);
 			$("#info-PobDesocup").html(feature.properties.PobDesocup);
 			$("#info-DensPobAvg").html(feature.properties.DensPobAvg);
 			
@@ -74,7 +109,7 @@ angular.module('dotmxApp')
 			circleLayer.addTo(map);
 			estaciones.bringToFront();
 			
-			layer.bindPopup(contenthtml).openPopup();
+			/*layer.bindPopup(contenthtml).openPopup();*/
 			
 			$(".leaflet-popup-close-button").click( function() {
 				circleLayer.clearLayers();
