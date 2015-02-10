aca11ab667ce56c49149afd4683e701b6fa868dd
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2015-02-10 00:23:21 -0600

    agregando nuevos geojson con la codificación arreglada y nuevas ciudades (ags y pachuca)

--- new/scripts/controllers/main.js
+++ new/scripts/controllers/main.js
@@ -28,10 +28,10 @@ angular.module('dotmxApp').controller('MainCtrl', function ($scope,$document) {
 			print(estaciones_puebla, lineas_puebla, city);
 			map.setView([19.044918668412617, -98.20747375488281], 13);
 		} else if(city == "gdl") {
-			print(estaciones_gdl, lineas_gdl, city);
+			print(estaciones_gdl, lineas_chihuahua, city); //cambiar las lineas
 			map.setView([20.674929132304698, -103.35479378700256], 13);
 		} else if(city == "mty") {
-			print(estaciones_mty, lineas_mty, city);
+			print(estaciones_mty, lineas_chihuahua, city); //cambiar las lineas
 			map.setView([25.68713198895331, -100.33032417297363], 13);
 		} else if(city == "juarez") {
 			print(estaciones_juarez, lineas_juarez, city);
@@ -42,7 +42,13 @@ angular.module('dotmxApp').controller('MainCtrl', function ($scope,$document) {
 		} else if(city == "chihua") {
 			print(estaciones_chihuahua, lineas_chihuahua, city);
 			map.setView([28.642690467330326, -106.08458518981934], 13);
-		}			
+		} else if(city == "aguas") {
+			print(estaciones_aguas, lineas_chihuahua, city); //cambiar las lineas
+			map.setView([21.876951611919733, -102.3012113571167], 13);
+		} else if(city == "pachuca") {
+			print(estaciones_pachuca, lineas_chihuahua, city); //cambiar las lineas
+			map.setView([20.117114283545682, -98.74726295471191], 13);
+		}				
 	});
 	
 	L.mapbox.accessToken = 'pk.eyJ1IjoiY2Fhcmxvc2h1Z28xIiwiYSI6IklmZGNsNmMifQ.JJksWU3hBP-Vd3S9WtjFsA';
