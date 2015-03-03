e52dd2de4d5c4ad957c1e96cc38102d86f094ebc
Mario Flores <pokazzam@gmail.com>
2015-03-02 18:30:51 -0600

    name or file changed

--- new/scripts/directives/dot-directives.js
+++ new/scripts/directives/dot-directives.js
@@ -15,37 +15,37 @@ angular.module('dotmxApp')
 })
 .directive('mainSection', function () {
 	return {
-		templateUrl: 'views/main_section.html',
+		templateUrl: 'views/sections/main_section.html',
 		restrict: 'E',
 	};
 })
 .directive('projectSection', function () {
 	return {
-		templateUrl: 'views/project_section.html',
+		templateUrl: 'views/sections/project_section.html',
 		restrict: 'E',
 	};
 })
 .directive('toolsSection', function () {
 	return {
-		templateUrl: 'views/tools_section.html',
+		templateUrl: 'views/sections/tools_section.html',
 		restrict: 'E',
 	};
 })
 .directive('guideSection', function () {
 	return {
-		templateUrl: 'views/guide_section.html',
+		templateUrl: 'views/sections/guide_section.html',
 		restrict: 'E',
 	};
 })
 .directive('strategiesSection', function () {
 	return {
-		templateUrl: 'views/strategies_section.html',
+		templateUrl: 'views/sections/strategies_section.html',
 		restrict: 'E',
 	};
 })
 .directive('modelSection', function () {
 	return {
-		templateUrl: 'views/model_section.html',
+		templateUrl: 'views/sections/model_section.html',
 		restrict: 'E',
 	};
 });
